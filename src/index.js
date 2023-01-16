cd5c96f8e3ae25551bcb497480d1d24a3a79d292
smerch88 <maxarmyk@gmail.com>
2023-01-16 20:21:54 +0200

    remove persistor

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Container, MantineProvider } from '@mantine/core';
 import { MyGlobalStyles } from 'global.Styled';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <MantineProvider
-        withNormalizeCSS
-        withGlobalStyles
-        theme={{ colorScheme: 'dark' }}
-      >
-        <MyGlobalStyles />
-        <Container>
-          <App />
-        </Container>
-      </MantineProvider>
-    </PersistGate>
+    <MantineProvider
+      withNormalizeCSS
+      withGlobalStyles
+      theme={{ colorScheme: 'dark' }}
+    >
+      <MyGlobalStyles />
+      <Container>
+        <App />
+      </Container>
+    </MantineProvider>
   </Provider>
 );
